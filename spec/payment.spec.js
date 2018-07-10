a3a4ed7cfd57d1fc0b47a8e5e3436ed3edb07b30
avoram01 <andrea.vorametsanti@tufts.edu>
2018-07-10 15:40:32 +0800

    test: happy case for form a

--- spec/payment.spec.js
+++ spec/payment.spec.js
@@ -6,9 +6,9 @@ const Nightmare = require( "nightmare" ),
         console.error( "Test-runner failed:", err );
       },
       browser = new Nightmare({
-          openDevTools: {
-            mode: 'detach'
-          },
+        //   openDevTools: {
+        //     mode: 'detach'
+        //   },
           height: 768,
           width: 1024,
           show: true,
@@ -19,27 +19,9 @@ const Nightmare = require( "nightmare" ),
 
       jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
 
-// const JasmineConsoleReporter = require('jasmine-console-reporter');
-// const reporter = new JasmineConsoleReporter({
-//     colors: 1,           // (0|false)|(1|true)|2
-//     cleanStack: 1,       // (0|false)|(1|true)|2|3
-//     verbosity: 4,        // (0|false)|1|2|(3|true)|4
-//     listStyle: 'indent', // "flat"|"indent"
-//     activity: false,     // boolean or string ("dots"|"star"|"flip"|"bouncingBar"|...)
-//     emoji: true,
-//     beep: true
-// });
-//
-// jasmine.env.clearReporters();
-// jasmine.addReporter(reporter);
-// jasmine.execute();
-
 //describe("10 day free trial funnel", function() {
 
     describe("A spec to ensure user info is filled", function() {
-        // var timeoutPromise = new Promise(function(resolve, reject){
-        //     setTimeout(resolve, 5000);
-        // });
         // start up with the blank list
         beforeEach( function(done)  {
             
@@ -57,16 +39,16 @@ const Nightmare = require( "nightmare" ),
         // disconnect and close Electron process
         afterEach( function(done) {
 
-            // browser.end().then(() => {
-            //     console.log("afterEach");
-            //     done()
-            // });
+            browser.end().then(() => {
+                console.log("afterEach");
+                done()
+            });
 
         });
 
         it ("should submit with all forms filled", function(done) {
-            console.log("just name"); 
             browser
+
                 // inputting name
                 .wait('#headerNav-cta span')
                 .click('#headerNav-cta span')
@@ -81,58 +63,109 @@ const Nightmare = require( "nightmare" ),
                 .wait("#theForm")
 
                 .then( function() {
+                    // var link = browser.path();
+                    // console.log(link);
                     return browser
                         .wait("#password")
                         .type("#password", "password8")
                         .wait("#password_verify")
                         .type("#password_verify", "password8")
                         .wait("#secret_question")
-                        
+                            
                         .evaluate(function() {
                             return $('select[id="secret_question"] option:contains("color")').val()
                         })
                         .then(function(value){
                             return browser.select('#secret_question', value)
                         })
+
                         .then(function (){
                             return browser
                                 .type("#secret_question_answer", "yellow")
                                 .wait("button[class='submit_button']")
                                 .click("button[class='submit_button']")
                                 .wait("#stripe-card-number")
-                                .then( function() {
-                                    return browser
-                                        .type("#name_on_card", "andrea vora")
+
+                                // CHANGING FUNNELS
+                                .goto("https://www.kidguard.com/funnel/payment/011/a")
+
+                                .evaluate(function () {
+                                    return location.href;
+                                })
+                                
+                                .then(function (url) {                                   
+                                    if (url.includes("a?")) {
+                                        console.log("funnel a");
+
+                                        return browser 
+                                            
+                                        .type("input[name='name_on_card']", "andrea vora")
                                         .wait(1000)
-                                        // .type("input[name='cardnumber']", "4242424242424242")
-                                        .type("#stripe-card-number input", "4242424242424242")
+                                        .type("input[name='card_number']", "4242424242424242")
                                         .wait(1000)
-                                        .type("#stripe-card-cvc input", "424")
+                                        .type("input[name='cvv']", "424")
                                         .wait(1000)
-                                        .type("#stripe-card-expiry input", "424")
+                                        .type("input[name='expiration_month']", "4")
+                                        .type("input[name='expiration_year']", "24")
+                                      
+                                        // SAME AS FUNNEL B
                                         .type("#address1", "123 Main Street")
                                         .type("#city", "Smallville")
                                         .type("#state", "CA")
-                                        .type("#stripe-postal-code", "12345")
+                                      
+                                      
+                                        .type("input[name='zipcode']", "12345")
                                         .click("input[type='checkbox']")
+                                        .wait(10000)
+                                      
+
+
+                                    } else if (url.includes("b?")) {
+                                        console.log("funnel b");
+
+                                        return browser
+                                            .type("#name_on_card", "andrea vora")
+                                            .wait(1000)
+                                            .type("#stripe-card-number input", "4242424242424242")
+                                            .wait(1000)
+                                            .type("#stripe-card-cvc input", "424")
+                                            .wait(1000)
+                                            .type("#stripe-card-expiry input", "424")
+                                            .type("#address1", "123 Main Street")
+                                            .type("#city", "Smallville")
+                                            .type("#state", "CA")
+                                            .type("#stripe-postal-code", "12345")
+                                            .click("input[type='checkbox']")
+
+                                    } else if (url.includes("c?")) {
+                                        console.log("funnel c");
+                                    } else if (url.includes("d?")) {
+                                        console.log("funnel d");
+                                    }
+
+                                    
+
+                                    
+
+
+                                        // TODO trying to confirm info is proper
                                         // .wait(30000)
 
-                                        console.log("friday")
-                                        var selector = document.getElementByClass("btn -btn-success padding-30-lr padding-15-tb -font-lg")
-                                        .then( function() {
-                                            console.log(selector)
-                                            console.log("hello")
-                                        })
+                                        // console.log("friday")
+                                        // var selector = document.getElementByClass("btn -btn-success padding-30-lr padding-15-tb -font-lg")
+                                        // .then( function() {
+                                        //     console.log(selector)
+                                        //     console.log("hello")
+                                        // })
                                         
                                         // expect($("button[ng-click='vm.submit()']")).toHaveAttr('disabled')
-                                        expect(selector).hasAttribute("disabled").toBe(true)
+                                        // expect(selector).hasAttribute("disabled").toBe(true)
                                         // expect(document.getElementById("myBtn")).toContain('')
                                 })
                         })
-                        
+                            
                 })
-
-
+                
                 .then(( ) => {
                     console.log("finished")
                     done();
@@ -176,7 +209,7 @@ const Nightmare = require( "nightmare" ),
     //           .end();
     //     });
     // });
-//});
+
 
 // describe("Try now funnel", function() {
 //
